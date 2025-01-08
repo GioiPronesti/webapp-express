7e1073b868f175676fe65a15294c51b69130f035
Gioi.Pronesti <gioi.pronesti@gmail.com>
2025-01-08 19:08:27 +0100

    add: middlewares function on app.js

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const notFound = require("./middlewares/notFound");
+const errorsHandler = require("./middlewares/errorsHandler");
 const movieRouter = require("./routers/movieRouter");
 
 app.use(express.static("public"));
-
-app.use("/api/movies", movieRouter);
 
 app.get("/", (req, res) => {
   console.log("main root");
   res.send("response server main root");
 });
+
+app.use("/api/movies", movieRouter);
+
+app.use(errorsHandler);
+
+app.use(notFound);
 
 app.listen(port, () => {
   console.log("server is running");
