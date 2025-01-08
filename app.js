58832fc9879359191a4472925abe153208abb2ee
Gioi.Pronesti <gioi.pronesti@gmail.com>
2025-01-08 15:35:52 +0100

    web app express movies first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  console.log("main root");
+  res.send("response server main root");
+});
+
+app.listen(port, () => {
+  console.log("server is running");
+  res.send("server listening on port 3000!");
+});
